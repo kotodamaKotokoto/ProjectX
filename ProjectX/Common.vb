58e884c7f7266498a8332efc6d0f8d12ab64ac8a
Kotodama <kodama.117.150@gmail.com>
2021-01-11 21:51:08 +0900

    add common module to show String message in messageBox

---
+++ ProjectX/Common.vb
@@ -0,0 +1,11 @@
+﻿
+Module Common
+
+    Public Sub ShowMessage(str As String)
+        If Not str Is Nothing Then
+            MsgBox(str)
+        Else
+            MsgBox("文字列はNothing です！")
+        End If
+    End Sub
+End Module
