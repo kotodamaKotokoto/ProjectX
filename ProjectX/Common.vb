652f1050f144024a4b5f007285ff4ce1be853313
Kotodama <kodama.117.150@gmail.com>
2021-01-28 22:10:23 +0900

    add GetCSV() method to read csv

--- ProjectX/Common.vb
+++ ProjectX/Common.vb
@@ -12,4 +12,38 @@ Module Common
             MsgBox("文字列はNothing です！")
         End If
     End Sub
+
+    Public Function GetCSV(file_path As String) As List(Of String)
+        Dim list As New List(Of String)
+
+        ' csv ファイルから読み込み処理
+        Using textFile As New FileIO.TextFieldParser(file_path) ' -- デフォルト encoding は UTF8
+            ' --- デリミターをタブと定義する
+            textFile.TextFieldType = FileIO.FieldType.Delimited
+            textFile.SetDelimiters(vbTab)    ' -- カンマ区切りの場合はカッコ内を "," にします
+            ' --- 行を文字型配列（currentRow）に読み込み、各列を DataGridView に格納する
+            Dim currentRow As String() ' -- 文字型配列
+            Dim myRow As Integer = 0
+            Dim myCol As Integer = 0
+            ' ---▼▼ 行ループ
+            While Not textFile.EndOfData
+                currentRow = textFile.ReadFields() ' -- １行を文字型配列に格納
+                Dim currentColumn As String
+                ' ---▼ 列ループ：１行分の列を埋める
+                For Each currentColumn In currentRow
+
+                    Dim str As String() = currentColumn.Split(",")
+                    For Each item In str
+                        list.Add(item)
+                    Next
+                    myCol += 1
+                Next ' --▲ 列ループ
+                myCol = 0
+                myRow += 1
+            End While ' --▲▲ 行ループ
+            ' --- 入力ファイルを閉じる
+        End Using
+
+        GetCSV = list
+    End Function
 End Module
--- ProjectX/Form1.vb
+++ ProjectX/Form1.vb
@@ -3,40 +3,7 @@
     Private forture_telling_list As New List(Of String)
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-
-
-        ' csv ファイルから読み込み処理
-        Dim textFile As FileIO.TextFieldParser ' -- 入力するファイル
-        ' --- 入力ファイルを開く
-        textFile = New FileIO.TextFieldParser("..\../csv/forture_telling.csv") ' -- デフォルト encoding は UTF8
-        ' --- デリミターをタブと定義する
-        textFile.TextFieldType = FileIO.FieldType.Delimited
-        textFile.SetDelimiters(vbTab)    ' -- カンマ区切りの場合はカッコ内を "," にします
-        ' --- 行を文字型配列（currentRow）に読み込み、各列を DataGridView に格納する
-        Dim currentRow As String() ' -- 文字型配列
-        Dim myRow As Integer = 0
-        Dim myCol As Integer = 0
-        ' ---▼▼ 行ループ
-        While Not textFile.EndOfData
-            currentRow = textFile.ReadFields() ' -- １行を文字型配列に格納
-            Dim currentColumn As String
-            ' ---▼ 列ループ：１行分の列を埋める
-            For Each currentColumn In currentRow
-
-                Dim str As String() = currentColumn.Split(",")
-                For Each item In str
-                    forture_telling_list.Add(item)
-                Next
-                myCol += 1
-            Next ' --▲ 列ループ
-            myCol = 0
-            myRow += 1
-        End While ' --▲▲ 行ループ
-        ' --- 入力ファイルを閉じる
-        textFile.Close()
-
-
+        forture_telling_list = GetCSV("..\../csv/forture_telling.csv")
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
